86a90925bfd49fd86342c20d5e33f1bf0c3f4958
Giremuu <giremu.gp@gmail.com>
2025-10-26 16:20:54 +0100

    Some changes + Disgusting comp pages

--- style/js/content.js
+++ style/js/content.js
@@ -3,18 +3,59 @@
 // ================================
 export const bioContents = {
   aboutMe: `
-    <p> Diplômé en <strong>cybersécurité</strong> puis passionné par les systèmes, les réseaux et l’automatisation, je <strong>construis pas à pas un parcours technique solide, entre études, alternance et projets personnels.</strong><br><br>Mon objectif : M’installer durablement pour mettre <strong>mes compétences au service de ceux qui me feront confiance.</strong></p>
+    <p> Diplômé en <strong>cybersécurité</strong> puis passionné par les systèmes, les réseaux et l’automatisation, je <strong>construis pas à pas un parcours technique solide entre études, alternance et projets personnels.</strong><br><br>Mon objectif : M’installer durablement pour mettre <strong>mes compétences au service de ceux qui me feront confiance.</strong></p>
   `,
   
   parcours: `<div id="parcours-root"></div>`,
-  certifications: `<p> Soon je le jure </p>`,
+
+  competences: `
+<div class="skills-list">
+    <details class="skill-item" open>
+      <summary class="skill-summary">Systèmes & Réseau</summary>
+      <p class="skill-content">
+        Windows Server (AD, DHCP, DNS...) / Modèles TCP-IP & OSI / Virtualisation (VMware) / Sauvegardes / Gestion de tickets (GLPI)
+      </p>
+    </details>
+
+    <details class="skill-item">
+      <summary class="skill-summary">Cybersécurité</summary>
+      <p class="skill-content">
+        Supervision (Zabbix, LibreNMS) / Bastion (Wallix, Teleport) / VPN / SSH - RDP / Analyse réseau (Wireshark) / RGPD
+      </p>
+    </details>
+
+    <details class="skill-item">
+      <summary class="skill-summary">Automatisation</summary>
+      <p class="skill-content">
+        Script PowerShell / Script Shell / Ansible
+      </p>
+    </details>
+
+    <details class="skill-item">
+      <summary class="skill-summary">Développement & Web</summary>
+      <p class="skill-content">
+        HTML - CSS - JS / Base de données (Firebase, PHP, MariaDB) / Python / Java
+      </p>
+    </details>
+
+    <details class="skill-item">
+      <summary class="skill-summary">Environnements</summary>
+      <p class="skill-content">
+        Windows 10 / Windows 11 / Windows Server / Android / Linux - Debian / Linux - Kali / Linux - Ubuntu
+      </p>
+    </details>
+  </div>
+  `,
+
   projets: `<p> Soon je le jure aussi </p>`,
+
+
   contact: `
     <div class="contact-actions" style="display:flex;gap:.6rem;flex-wrap:wrap">
       <a id="email-link" class="cat-btn" href="#" rel="nofollow noopener">Me contacter par email</a>
       <button id="reveal-email" class="cat-btn" data-b64="Z2lyZW11LmpwQGdtYWlsLmNvbQ==">Afficher l’email</button>
       <button id="copy-email" class="cat-btn" hidden>Copier l’email</button>
-      <button class="cat-btn"> <p style="margin-top:.6rem">Me retrouver sur GitHub : <a href="https://github.com/Giremuu" target="_blank" rel="noopener">Giremuu</a></p></button>
+      <button class="cat-btn"> <p style="margin:0rem">Me retrouver sur GitHub : <a href="https://github.com/Giremuu" target="_blank" rel="noopener">Giremuu</a></p></button>
     </div>
   `
 };
--- style/js/timeline-data.js
+++ style/js/timeline-data.js
@@ -4,7 +4,7 @@
 export const timelineData = {
   2022: {
     titre: "Baccalauréat",
-    tags: ["Saint-Nazaire - Lycée Aristide Briand"],
+    tags: ["Scolaire : Saint-Nazaire - Lycée Aristide Briand"],
     html: `
       <ul>
         <li>Baccalauréat Général option Mathématique et Numérique Science de l'Ingénieur obtenu</li>
@@ -13,7 +13,7 @@ export const timelineData = {
   },
   2023: {
     titre: "Développement - Université",
-    tags: ["Nantes - Université de Science option Informatique parcours Développement"],
+    tags: ["Scolaire : Nantes - Université de Science option Informatique parcours Développement"],
     html: `
       <ul>
         <li>Année en développement informatique</li>
@@ -22,31 +22,32 @@ export const timelineData = {
   },
   2024: {
     titre: "1ere année BTS",
-    tags: ["Saint-Nazaire - IIA","Montoir-de-Bretagne - Groupe IDEA"],
+    tags: ["Scolaire : Saint-Nazaire - Institut Informatique Appliqué","Stage : Montoir-de-Bretagne - Groupe IDEA"],
     html: `
       <ul>
         <li>1ere Année en BTS SIO SISR</li>
-        <li>+ Stage en service Informatique Support chez GROUPE IDEA</li>
+        <li>+ Stage en service Informatique Support</li>
       </ul>
     `
   },
   2025: {
     titre: "2eme année BTS",
-    tags: ["Saint-Nazaire - IIA", "Montoir-de-Bretagne - Groupe IDEA"],
+    tags: ["Scolaire : Saint-Nazaire - Institut Informatique Appliqué", "Alternance : Montoir-de-Bretagne - Groupe IDEA"],
     html: `
       <ul>
         <li>Obtention de mon BTS SIO SISR</li>
-        <li>Alternance en service Informatique Support chez GROUPE IDEA</li>
+        <li>+ Alternance en service Informatique Support</li>
+        <li>+ Certifications MOOC RGPD - CNIL</li>
       </ul>
     `
   },
   2026: {
     titre: "Licence Informatique",
-    tags: ["Saint-Nazaire - IIA", "Montoir-de-Bretagne - Groupe IDEA"],
+    tags: ["Scolaire : Saint-Nazaire - Institut Informatique Appliqué", "Alternance : Montoir-de-Bretagne - Groupe IDEA"],
     html: `
       <ul>
         <li>Obtention de ma Licence STS Option Informatique parcours Cybersécurité</li>
-        <li>Alternance en service Infrastructure et Réseau</li>
+        <li>+ Alternance en service Infrastructure et Réseau</li>
       </ul>
     `
   }
